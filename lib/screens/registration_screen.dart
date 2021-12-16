b7cad1f67eb7f9411312f3509a8ae98d53b4911f
jiaxen <jiaxen.lau@gmail.com>
2021-12-16 17:57:51 +0000

    Log in and progress hud

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../constants.dart';
 import 'chat_screen.dart';
+import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -20,75 +21,80 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo_hero',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ProgressHUD(
+        child: Builder(
+          builder: (context) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Hero(
+                    tag: 'logo_hero',
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 48.0,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    decoration:
+                        kTextFieldDecoration.copyWith(hintText: 'Enter your Email'),
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your password'),
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                  TextButton(
+                    style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                            EdgeInsets.all(15)),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            Colors.lightBlueAccent),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32.0),
+                                side: BorderSide(color: Colors.lightBlueAccent)))),
+                    child: Text('Register'),
+                    onPressed: () async {
+                      final progress = ProgressHUD.of(context);
+                      progress?.show();
+                      final userEmail = email;
+                      final userPassword = password;
+                      if (userEmail != null && userPassword != null) {
+                        try {
+                          final newUser =
+                              await _auth.createUserWithEmailAndPassword(
+                                  email: userEmail, password: userPassword);
+                          Navigator.pushNamed(context, ChatScreen.id);
+                        } catch (e) {
+                          print(e);
+                        }
+                      }
+                      progress?.dismiss();
+                    },
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration:
-                  kTextFieldDecoration.copyWith(hintText: 'Enter your Email'),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your password'),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            TextButton(
-              style: ButtonStyle(
-                  padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32.0),
-                          side: BorderSide(color: Colors.lightBlueAccent)
-                      )
-                  )
-              ),
-              child: Text('Register'),
-              onPressed: () async {
-                final userEmail = email;
-                final userPassword = password;
-                if (userEmail != null && userPassword != null) {
-                  try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: userEmail, password: userPassword);
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  } catch (e) {
-                    print(e);
-                  }
-                };
-                // print(email);
-                // print(password);
-              },
-            ),
-          ],
+            )
         ),
       ),
     );
