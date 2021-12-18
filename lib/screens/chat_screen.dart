d8240791cbb74e367dd74284c001cd35c42dfcd0
jiaxen <jiaxen.lau@gmail.com>
2021-12-18 01:44:55 +0000

    Chat screen logic

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flashchat2/constants.dart';
@@ -11,8 +12,11 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _firestore = FirebaseFirestore.instance;
+  final textEditingController = TextEditingController();
   final _auth = FirebaseAuth.instance;
   late User loggedInUser;
+  String? messageText;
 
   @override
   void initState() {
@@ -50,6 +54,26 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder(
+                stream: _firestore.collection('messages').snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    List<Widget> displayedMessages = [];
+                    final messages = snapshot.data!.docs;
+                    for (DocumentSnapshot message in messages) {
+                      final messageText = message['text'];
+                      final messageSender = message['sender'];
+                      displayedMessages.add(MessageBubble(
+                        sender: messageSender,
+                        text: messageText,
+                      ));
+                    }
+                    return Expanded(
+                        child: ListView(children: displayedMessages));
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                }),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -57,15 +81,18 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: textEditingController,
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
-                  FlatButton(
+                  TextButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      textEditingController.clear();
+                      _firestore.collection('messages').add(
+                          {'sender': loggedInUser.email, 'text': messageText});
                     },
                     child: Text(
                       'Send',
@@ -78,6 +105,40 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({this.text, this.sender});
+
+  final String? text;
+  final String? sender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end, children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(25,0,0,0),
+          child: Material(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                child: Text(
+                  '$text',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              elevation: 3,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Colors.lightBlue),
+        ),
+        SizedBox(height: 5),
+        Text('sent by $sender',
+            style: const TextStyle(color: Colors.black45, fontSize: 12)),
+      ]),
     );
   }
 }
